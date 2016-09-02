a5e12ca1d28cdef90ae7483df4cd0d22d8ca4a45
Your Name <you@example.com>
2016-09-02 03:59:16 +0000

    before carewave login doneCD root/src

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def profile_img(user)
+    unless user.provider.blank?
+      img_url = user.image_url
+    else
+      img_url = 'no_image.png'
+    end
+    image_tag(img_url, alt: user.name)
+  end
 end
